ee562e9e8804070bb57fdfbeca6370f21a6c44d5
Andreia Dias <andreiadias2311@gmail.com>
2026-02-19 16:43:34 -0300

    lista_tuplas.py

---
+++ lista_tuplas.py
@@ -0,0 +1,20 @@
+# Lista e tuplas
+#As listas e tuplas são estruturas de dados em Python que armazenam coleções de elementos. A principal diferença 
+# entre elas reside na mutabilidade. Listas são mutáveis, 
+# o que significa que seus elementos podem ser modificados após a criação. 
+# Em contrapartida, tuplas são imutáveis, tornando seus elementos fixos após a declaração.
+
+# #range é para sequencia
+# for variavel in range(5):
+#     print(1)
+
+#Numero procurado
+lista_numeros = [1,2,3,4,5]
+print(lista_numeros)
+
+# .append() adicionar
+# .insert() adiciona um elemento na posição especifica
+# .remove() remove atraves do valor
+# .pop() remove atraves do indice
+
+#tuplas são imutáveis

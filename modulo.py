8f19114924c91e93f21497b57137c82e4f21b0af
Andreia Dias <andreiadias2311@gmail.com>
2026-03-03 18:20:32 -0300

    modulo.py

---
+++ modulo.py
@@ -0,0 +1,18 @@
+#square root = raiz quadrada
+import math 
+print(math.sqrt(2))
+
+#arredonda para um numero menor ou igual ao numero <= resultado:7
+print(math.floor(7.8))
+
+#arredonda para cima >= resultado:8
+print(math.ceil(7.8))
+
+
+# import contar
+
+# numero1 = int(input("Digite o primeiro numero "))
+# numero2 = int(input("digite o segundo número "))
+
+# soma = contar.somarnumero(numero1, numero2)
+# print(soma)
